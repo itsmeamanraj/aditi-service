7cc51bdb1404d368ff67708b4675caa08b2fbaf8
NayanRaval <ravalnayan029@gmail.com>
2025-07-12 15:01:45 +0530

    Refactor service detail methods and update view to enhance tab content rendering

--- app/Controllers/Service.php
+++ app/Controllers/Service.php
@@ -20,14 +20,15 @@ class Service extends BaseController
         }
     }
 
-    public function index(){
+    public function index()
+    {
         $serviceModel = new ServiceModel();
         $user_id = session()->get('user_id');
         $services = $serviceModel->where('user_id', $user_id)->findAll();
         return view('service-list', ['service' => $services]);
     }
 
-    public function servicedetail($service_id)
+    public function servicedetailnew($service_id)
     {
         $user_id = session()->get('user_id');
         $ServiceModel = new ServiceModel();
@@ -45,9 +46,15 @@ class Service extends BaseController
             $tabContent[$item['tab_id']] = $item['user_input'];
         }
 
-        return view('service-detail', ['service_detail'=> $service, 'service_tab' => $serviceTabs, 'getTabServicecontet'=> $tabContent]);
+        return view('service-detail', ['service_detail' => $service, 'service_tab' => $serviceTabs, 'getTabServicecontet' => $tabContent]);
     }
 
-
+    public function servicedetail($service_id)
+    {
+          $ServiceModel = new ServiceModel();
+        $ServiceModel1 = $ServiceModel->gettabservice($service_id);
+        $getTabServicecontet = $ServiceModel->getTabServicecontet($service_id);
+        
+        return view('service-detail', ['service_detail'=> $ServiceModel1, 'getTabServicecontet'=> $getTabServicecontet]);
+    }
 }
-?>
